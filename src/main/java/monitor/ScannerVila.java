a3ce4464c4be9b84ed6467f3b59e001aff0be89a
Helio Franca <heliordf@hotmail.com>
2021-04-15 19:13:54 -0300

    Ajustes

--- src/main/java/monitor/ScannerVila.java
+++ src/main/java/monitor/ScannerVila.java
@@ -8,7 +8,7 @@ public class ScannerVila {
 
 	public static void iniciarScan() {
 		String vila01 = "https://ts100.x10.international.travian.com/dorf1.php?newdid=22116&";
-//		String vila02 = "https://ts100.x10.international.travian.com/dorf1.php?newdid=30354&";
+		String vila02 = "https://ts100.x10.international.travian.com/dorf1.php?newdid=30354&";
 //		String vila03 = "https://ts100.x10.international.travian.com/dorf1.php?newdid=32635&";
 //		String vila04 = "https://ts100.x10.international.travian.com/dorf1.php?newdid=34823&";
 //		String vila05 = "https://ts100.x10.international.travian.com/dorf1.php?newdid=36987&";
@@ -27,6 +27,7 @@ public class ScannerVila {
 //		String mercado02 = "https://ts100.x10.international.travian.com/build.php?newdid=30354&id=32&gid=17";
 //		GerarTropas.loopParaCriarTropas("30", quartel02, vila02);
 // 		EnviarRecursos.iniciarEnvio(vila02, recebedora, mercado02);
+		MelhorarCamposV2.estrutura(vila02, "https://ts100.x10.international.travian.com/build.php?id=28&gid=11", 19);
 //
 //		String quartel03 = "https://ts100.x10.international.travian.com/build.php?newdid=32635&id=30&gid=19";
 //		MelhorarCamposV2.recurso(vila03);
